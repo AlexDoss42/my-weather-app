0fab13bd0625e86f1748c111c99f1bad164be8d4
AlexDoss42 <alex.doss42@gmail.com>
2022-02-17 18:32:58 -0700

    sets up some state and effect on app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    })
+
+    console.log("latitude is: ", lat);
+    console.log("longitude is: ", long);
+  }, [lat, long]);
+
   return (
     <div className="App">
       
