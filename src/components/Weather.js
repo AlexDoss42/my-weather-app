2a9dcde512d65fd274b2709e88d70986405038a7
AlexDoss42 <alex.doss42@gmail.com>
2022-02-22 23:18:35 -0700

    adds some details to the card

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import { Card } from 'semantic-ui-react'
 
 function Weather ({ weatherData }) {
@@ -6,7 +7,14 @@ function Weather ({ weatherData }) {
     return (
     <Card>
         <Card.Content>
-            <Card.Header className="header">{weatherData.name}</Card.Header>
+            <Card.Header className="header">City Name: {weatherData.name}</Card.Header>
+            <p>Temprature: {weatherData.main.temp} &deg;C</p>
+            <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+            <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+            <p>Description: {weatherData.weather[0].main}</p>
+            <p>Humidity: {weatherData.main.humidity} %</p>
+            <p>Day: {moment().format('dddd')}</p>
+            <p>Date: {moment().format('LL')}</p>
         </Card.Content>
     </Card>
     )
