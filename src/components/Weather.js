54a4481dd4ddff709376b35e044df908a444506b
AlexDoss42 <alex.doss42@gmail.com>
2022-02-22 23:14:44 -0700

    adds a card to the weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react'
 
-function Weather () {
+function Weather ({ weatherData }) {
 
     return (
-        <div>
-            Weather!!!
-        </div>
+    <Card>
+        <Card.Content>
+            <Card.Header className="header">{weatherData.name}</Card.Header>
+        </Card.Content>
+    </Card>
     )
 }
 
