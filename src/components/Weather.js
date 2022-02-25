3c5943f9bac6f552021d06645f91a182a9ffefc8
AlexDoss42 <alex.doss42@gmail.com>
2022-02-24 23:58:41 -0700

    cleaning up props in weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 import './weather.css';
 import { Button, Dimmer, Loader } from 'semantic-ui-react';
 
-function Weather ({ weatherData }) {
+function Weather ({ coordinates }) {
 
     const [lat, setLat] = useState([]);
     const [long, setLong] = useState([]);
@@ -33,27 +33,28 @@ function Weather ({ weatherData }) {
     }
 
     return (
+      <>
         {(typeof data.main != 'undefined') ? (
         <div className="main">
 
             <div className='top'>
-                <p className="header">{weatherData.name}</p>
+                <p className="header">{data.name}</p>
                 <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
             </div>
 
             <div className="flex">
                 <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
-                <p className="description">{weatherData.weather[0].main}</p>
+                <p className="description">{data.weather[0].main}</p>
             </div>
 
             <div className="flex">
-                <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
-                <p className="temp">Humidity: {weatherData.main.humidity} %</p>
+                <p className="temp">Temprature: {data.main.temp} &deg;C</p>
+                <p className="temp">Humidity: {data.main.humidity} %</p>
             </div>
 
             <div className="flex">
-                <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-                <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+                <p className="sunrise-sunset">Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+                <p className="sunrise-sunset">Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
             </div>
 
         </div>) : (
@@ -63,6 +64,8 @@ function Weather ({ weatherData }) {
               </Dimmer>
            </div>
          )}
+
+    </>
     )
 }
 
